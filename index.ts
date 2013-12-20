0015a9c5736184e35d62065ceb5edfbd7e1e48ae
Hans Malherbe <hans.malherbe@gmail.com>
2013-12-20 09:34:26 +0200

    remove DT defs

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 // Copyright (c) ComUnity 2013
 // hansm@comunity.co.za (Hans Malherbe)
 
-///<reference path="./typed/node/node.d.ts" />
-///<reference path="./typed/q/Q.d.ts" />
+///<reference path="../../typed/node/node.d.ts" />
+///<reference path="../../typed/q/Q.d.ts" />
 
 import Q = require('q')
 import stream = require('stream')
