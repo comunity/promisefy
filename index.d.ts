dc8ff670c03c94bdb6b60aaad9996fd59f215c68
Hans Malherbe <hans.malherbe@gmail.com>
2014-02-07 07:35:40 +0200

    types

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
 declare module 'promisefy' {
 	import stream = require('stream');
-	function fold<T>(x: Q.Promise<Q.Promise<T>>): Q.Promise<T>;
-	function fileExists(path: string): Q.Promise<boolean>;
-	function mkdirp(path: string): Q.Promise<string>;
-	function pipe(is: stream.ReadableStream, os: stream.WritableStream): Q.Promise<number[]>;
-	function writeFile(filename: string, data: any): Q.Promise<number[]>;
-	function readFile(filename: string): Q.Promise<NodeBuffer>;
+	export function fold<T>(x: Q.Promise<Q.Promise<T>>): Q.Promise<T>;
+	export function fileExists(path: string): Q.Promise<boolean>;
+	export function mkdirp(path: string): Q.Promise<string>;
+	export function pipe(is: stream.ReadableStream, os: stream.WritableStream): Q.Promise<number[]>;
+	export function writeFile(filename: string, data: any): Q.Promise<number[]>;
+	export function readFile(filename: string): Q.Promise<NodeBuffer>;
 }
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Copyright (c) ComUnity 2013
 // hansm@comunity.co.za (Hans Malherbe)
-///<reference path="../../typed/node/node.d.ts" />
-///<reference path="../../typed/q/Q.d.ts" />
+///<reference path="../typed/node/node.d.ts" />
+///<reference path="../typed/q/Q.d.ts" />
 var Q = require('q');
 
 var fs = require('fs');
